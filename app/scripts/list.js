bf57f5b142f5d286f7f3c0455d01512176a572ee
takeyama <takeyama@192.168.1.5>
2015-10-16 18:10:23 +0900

    script分離

---
+++ app/scripts/list.js
@@ -0,0 +1,46 @@
+$(function(){
+	$.ajax({
+		url: '/d/registration?f&l=50&n=1',
+		method: 'get',
+		dataType: 'json'
+	}).done(function( res ) {
+		if(res) {
+			setTable(res);
+		}
+	}).fail(function( jqXHR ) {
+	});
+});
+function setTable(res){
+	var temp = function(i, a, b, c, d){
+		var setVal = function(v){
+			return (v ? v : '<span style="color:#ccc;">(入力なし)</span>')
+		}
+		return '<tr>'+
+			'<td>'+ ( i + 1 ) +'</td>'+
+			'<td>'+ setVal(a) +'</td>'+
+			'<td>'+ setVal(b) +'</td>'+
+			'<td>'+ setVal(c) +'</td>'+
+			'<td>'+ setVal(d) +'</td>'+
+		'</tr>';
+	}
+	var array = [];
+	for (var i = 0, ii = res.feed.entry.length; i < ii; ++i) {
+		var entry = res.feed.entry[i];
+		var userinfo = entry.userinfo;
+		var favorite = entry.favorite;
+		var id;
+		var email;
+		var food;
+		var music;
+		if (userinfo) {
+			id = userinfo.id;
+			email = userinfo.email;
+		}
+		if (favorite) {
+			food = favorite.food;
+			music = favorite.music;
+		}
+		array.push(temp(i, id, email, food, music));
+	}
+	$('#list').find('tbody').html(array.join(''));
+}
