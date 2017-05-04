beb9c27b4bfdd1fd277a9f7d706528052405f3a0
takezaki <takezaki@virtual-tech.net>
2017-05-04 18:30:47 +0900

    update build

--- test/index.js
+++ test/index.js
@@ -1,41 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// identity function for calling harmony imports with the correct context
 /******/ 	__webpack_require__.i = function(value) { return value; };
-/******/
+
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
@@ -46,7 +46,7 @@
 /******/ 			});
 /******/ 		}
 /******/ 	};
-/******/
+
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
 /******/ 	__webpack_require__.n = function(module) {
 /******/ 		var getter = module && module.__esModule ?
@@ -55,22 +55,41 @@
 /******/ 		__webpack_require__.d(getter, 'a', getter);
 /******/ 		return getter;
 /******/ 	};
-/******/
+
 /******/ 	// Object.prototype.hasOwnProperty.call
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-var reflexContext = __webpack_require__(1);
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person__ = __webpack_require__(1);
+//var Person = require('./person');
+
+
+/* harmony default export */ __webpack_exports__["a"] = function() { 
+	var person = new __WEBPACK_IMPORTED_MODULE_0__person__["a" /* default */]('Steve');
+	return person.say();
+};
+
+//module.exports = main;
+ 
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+//var reflexContext = require('reflexContext');
 
 /*       */
 var Person = function Person(
@@ -81,36 +100,15 @@ var Person = function Person(
     	this.name = name;
 };
 
-Person.prototype.say = function say ()  {
+Person.prototype.say = function say ()    {
+    	return 'Hello, I\'m ' + this.name + '!!';
 //       reflexContext.log('Hello, I\'m ' + this.name + '!!');
-       alert('Hello, I\'m ' + this.name + '!!');
+//       alert('Hello, I\'m ' + this.name + '!!');
 
 };
-module.exports = Person;
 
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var reflexContext = function() {
-};
-
-reflexContext.get = function(param) {
-
-  var context = {};
-  context.p = param;
-  context.then = function(func1,func2) {
-    func1('check ='+this.p.url);
-  };
-
-  return context;
-}
-
-module.exports = reflexContext; 
+/* harmony default export */ __webpack_exports__["a"] = Person;
+//module.exports = Person;
 
 
 /***/ }),
@@ -119,13 +117,12 @@ module.exports = reflexContext;
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__person__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__main__ = __webpack_require__(0);
 
-var person = new __WEBPACK_IMPORTED_MODULE_0__person___default.a('Steve');
-person.say();
 
+alert(__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__main__["a" /* default */])());
+
+  
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=index.js.map
