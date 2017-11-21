6853d5d18691fe85f7b09ec939a3052cb84a98e8
takeyama <takeyama@takeyama-no-MacBook-Air.local>
2017-11-21 13:13:53 +0900

    作成中

--- src/components/quotation-form.js
+++ src/components/quotation-form.js
@@ -27,6 +27,10 @@ import {
 
 import {
 	DeliveryChargeModal,
+	ItemDetailsModal,
+	BasicConditionModal,
+	RemarksModal,
+	ManifestoModal,
 } from './quotation-modal'
 
 export default class QuotationForm extends React.Component {
@@ -38,9 +42,9 @@ export default class QuotationForm extends React.Component {
 		this.entry = this.props.entry
 		this.entry.customer = this.entry.customer || []
 		this.entry.billto = this.entry.billto || {}
-		this.entry.staff = this.entry.staff || {}
-		this.entry.work = this.entry.work || []
+		this.entry.basic_condition = this.entry.basic_condition || []
 		this.entry.item_details = this.entry.item_details || []
+		this.entry.remarks = this.entry.remarks || []
 		this.entry.manifesto = this.entry.manifesto || []
 
 		this.master = this.props.master
@@ -48,55 +52,20 @@ export default class QuotationForm extends React.Component {
 
 		this.formType = 'create'
 
+		this.selectItemDetails = null
+
+		this.modal = {
+			basic_condition: { data: {} },
+			item_details: { data: {} },
+			remarks: { data: {} },
+			manifesto: { data: {} }
+		}
+
 		this.sampleData()
 
 	}
 
 	sampleData() {
-
-		this.entry.item_details = [{
-			item_name: '保管料',
-			unit_name: '1坪',
-			unit: '月',
-			unit_price: '¥5,000',
-			remarks: '棚、ラックシリーズ(新品：500円／中古品：300円)支給品利用可能'
-		}, {
-			item_name: '保管料',
-			unit_name: '1パレット',
-			unit: '月',
-			unit_price: '¥2,500',
-			remarks: 'サイズ1,100×1,100'
-		}, {
-			item_name: '保管料',
-			unit_name: '1パレット',
-			unit: '月',
-			unit_price: '¥2,500',
-			remarks: 'パレットラック縦積み'
-		}, {
-			item_name: '入庫作業量',
-			unit_name: '入庫量',
-			unit: '1点',
-			unit_price: '¥10〜¥30',
-			remarks: 'バラ、アソート入庫、外装検品、品番確認、数量検品、棚格納'
-		}, {
-			item_name: '入庫作業量',
-			unit_name: '入庫量',
-			unit: '1箱',
-			unit_price: '¥50',
-			remarks: '外装検品、品番確認、内装未見、箱積み'
-		}, {
-			item_name: '入庫作業量',
-			unit_name: 'デバンニング',
-			unit: '20F',
-			unit_price: '¥18,000',
-			remarks: 'ハイキューブ + ¥3,000'
-		}, {
-			item_name: '入庫作業量',
-			unit_name: 'デバンニング',
-			unit: '40F',
-			unit_price: '¥33,000',
-			remarks: 'ハイキューブ + ¥3,000'
-		}]
 
 		this.entry.manifesto = [{
 			manifesto_code: 'dbox-016',
@@ -283,30 +252,30 @@ export default class QuotationForm extends React.Component {
 				customer_name_kana: 'アア',
 				address1: 'bbb'
 			}]
-			this.entry.work = [{
-				consignment_service: '委託される業務',
-				workflow: 'ロジスティクス業務'
+			this.entry.basic_condition = [{
+				title: '委託される業務',
+				condition: [{ content: 'ロジスティクス業務' }]
 			},{
-				consignment_service: '保管',
-				workflow: ['常温保管', '共益費、光熱費、バース使用料含む', '補償金不要']
+				title: '保管',
+				condition: [{ content: '常温保管'}, { content: '共益費、光熱費、バース使用料含む'}, { content: '補償金不要'}]
 			},{
-				consignment_service: '荷役',
-				workflow: ['入庫作業　数量検品、外観検品', '出荷作業　ピッキング、外観検品、梱包、出荷事務']
+				title: '荷役',
+				condition: [{ content: '入庫作業　数量検品、外観検品'}, { content: '出荷作業　ピッキング、外観検品、梱包、出荷事務'}]
 			},{
-				consignment_service: '配送',
-				workflow: ['宅急便、DM便等にて発送']
+				title: '配送',
+				condition: [{ content: '宅急便、DM便等にて発送'}]
 			},{
-				consignment_service: '保険',
-				workflow: ['発送：火災保険、保管：盗難、火災保険']
+				title: '保険',
+				condition: [{ content: '発送：火災保険、保管：盗難、火災保険'}]
 			},{
-				consignment_service: '決済',
-				workflow: ['毎月末締切、翌月20日　銀行振込み（日本郵便ご利用の場合）', '毎月末締切、翌月末日　銀行振込み']
+				title: '決済',
+				condition: [{ content: '毎月末締切、翌月20日　銀行振込み（日本郵便ご利用の場合）'}, { content: '毎月末締切、翌月末日　銀行振込み'}]
 			},{
-				consignment_service: '取扱商品',
-				workflow: ['◯◯']
+				title: '取扱商品',
+				condition: [{ content: '◯◯'}]
 			},{
-				consignment_service: '出荷データ',
-				workflow: ['伝票発行の際、お客様データの加工などは致しません。', '弊社指定のCSV、EXCELデータが必須となります。']
+				title: '出荷データ',
+				condition: [{ content: '伝票発行の際、お客様データの加工などは致しません。'}, { content: '弊社指定のCSV、EXCELデータが必須となります。'}]
 			}]
 			this.entry.billto.billto_code = _data.value
 			this.entry.billto.billto_name = _data.value
@@ -325,11 +294,40 @@ export default class QuotationForm extends React.Component {
 		console.log(_data)
 	}
 
-	selectWork(_data) {
-		console.log(_data)
+	showAddModal(_key) {
+		this.modal[_key].data = {}
+		this.modal[_key].type = 'add'
+		this.modal[_key].visible = true
+		this.forceUpdate()
 	}
-	removeWork(_data) {
-		console.log(_data)
+	showEditModal(_key, _data, _index) {
+		this.modal[_key].data = _data
+		this.modal[_key].index = _index
+		this.modal[_key].type = 'edit'
+		this.modal[_key].visible = true
+		this.forceUpdate()
+	}
+	closeModal(_key) {
+		this.modal[_key].visible = false
+		this.forceUpdate()
+	}
+	removeList(_key, _index) {
+		let array = []
+		for (let i = 0, ii = this.entry[_key].length; i < ii; ++i) {
+			if (i !== _index) array.push(this.entry[_key][i])
+		}
+		this.entry[_key] = array
+		this.forceUpdate()
+	}
+	addList(_key, _data) {
+		this.entry[_key].push(_data)
+		this.modal[_key].visible = false
+		this.forceUpdate()
+	}
+	updateList(_key, _data) {
+		this.entry[_key][this.modal[_key].index] = _data
+		this.modal[_key].visible = false
+		this.forceUpdate()
 	}
 
 	render() {
@@ -341,7 +339,39 @@ export default class QuotationForm extends React.Component {
 				<BilltoAddModal isShow={this.state.showBilltoAddModal} close={() => this.setState({ showBilltoAddModal: false })} add={(data) => this.addBillto(data)} />
 				<BilltoEditModal isShow={this.state.showBilltoEditModal} close={() => this.setState({ showBilltoEditModal: false })} data={this.billto} />
 				<DeliveryChargeModal isShow={this.state.showDeliveryChargeModal} close={() => this.setState({ showDeliveryChargeModal: false })} />
-
+				<ItemDetailsModal
+					isShow={this.modal.item_details.visible}
+					close={() => this.closeModal('item_details')}
+					add={(obj) => this.addList('item_details', obj)}
+					edit={(obj) => this.updateList('item_details', obj)}
+					data={this.modal.item_details.data}
+					type={this.modal.item_details.type}
+				/>
+				<BasicConditionModal
+					isShow={this.modal.basic_condition.visible}
+					close={() => this.closeModal('basic_condition')}
+					add={(obj) => this.addList('basic_condition', obj)}
+					edit={(obj) => this.updateList('basic_condition', obj)}
+					data={this.modal.basic_condition.data}
+					type={this.modal.basic_condition.type}
+				/>
+				<RemarksModal
+					isShow={this.modal.remarks.visible}
+					close={() => this.closeModal('remarks')}
+					add={(obj) => this.addList('remarks', obj)}
+					edit={(obj) => this.updateList('remarks', obj)}
+					data={this.modal.remarks.data}
+					type={this.modal.remarks.type}
+				/>
+				<ManifestoModal
+					isShow={this.modal.manifesto.visible}
+					close={() => this.closeModal('manifesto')}
+					add={(obj) => this.addList('manifesto', obj)}
+					edit={(obj) => this.updateList('manifesto', obj)}
+					data={this.modal.manifesto.data}
+					type={this.modal.manifesto.type}
+				/>
+				
 				<Tab eventKey={1} title="見積内容">
 
 					<Form name={this.props.name} horizontal data-submit-form>
@@ -431,16 +461,16 @@ export default class QuotationForm extends React.Component {
 							<Panel collapsible header="基本条件" eventKey="2" defaultExpanded={false}>
 
 								<CommonTable
-									name="work"
-									data={this.entry.work}
+									name="basic_condition"
+									data={this.entry.basic_condition}
 									header={[{
-										field: 'consignment_service',title: '条件名', width: '300px'
+										field: 'title',title: '条件名', width: '300px'
 									}, {
-										field: 'workflow', title: '条件内容', width: '800px'
+										field: 'condition', title: '条件内容', width: '800px'
 									}]}
-									edit={(data) => this.selectWork(data)}
-									add={() => this.setState({ showWorkModal: true })}
-									remove={(data) => this.removeWork(data)}
+									edit={(data, index) => this.showEditModal('basic_condition', data, index)}
+									add={() => this.showAddModal('basic_condition')}
+									remove={(data, index) => this.removeList('basic_condition', index)}
 								/>
 
 							</Panel>
@@ -461,9 +491,9 @@ export default class QuotationForm extends React.Component {
 									}, {
 										field: 'remarks',title: '備考', width: '500px'
 									}]}
-									edit={(data) => this.selectWork(data)}
-									add={() => this.setState({ showItemDetailsModal: true })}
-									remove={(data) => this.removeWork(data)}
+									edit={(data, index) => this.showEditModal('item_details', data, index)}
+									add={() => this.showAddModal('item_details')}
+									remove={(data, index) => this.removeList('item_details', index)}
 								/>
 
 							</Panel>
@@ -476,9 +506,9 @@ export default class QuotationForm extends React.Component {
 									header={[{
 										field: 'content',title: '備考'
 									}]}
-									edit={(data) => this.selectWork(data)}
-									add={() => this.setState({ showRemarksModal: true })}
-									remove={(data) => this.removeWork(data)}
+									edit={(data, index) => this.showEditModal('remarks', data, index)}
+									add={() => this.showAddModal('remarks')}
+									remove={(data, index) => this.removeList('remarks', index)}
 								/>
 
 							</Panel>
@@ -521,9 +551,9 @@ export default class QuotationForm extends React.Component {
 						}, {
 							field: 'special_unit_price', title: '特別販売価格・単価', width: '150px'
 						}]}
-						edit={(data) => this.selectWork(data)}
-						add={() => this.setState({ showManifestoModal: true })}
-						remove={(data) => this.removeWork(data)}
+						edit={(data, index) => this.showEditModal('manifesto', data, index)}
+						add={() => this.showAddModal('manifesto')}
+						remove={(data, index) => this.removeList('manifesto', index)}
 					/>
 				</Tab>
 				<Tab eventKey={3} title="庫内作業状況">
