9e3f71db90d4a728c6397d8545e4ea7d4f50c663
竹山恵悟 <takeyama@takeyamaMacBook-puro.local>
2018-01-09 15:59:26 +0900

    請求データ一覧の合計件数表示 #268

--- src/components/common.js
+++ src/components/common.js
@@ -1689,10 +1689,11 @@ export class CommonTable extends React.Component {
 			}
 
 			const isError = (obj && obj.is_error === true) ? 'isError' : ''
+			const isTotal = (obj && obj.is_total === true) ? 'isTotal' : ''
 			const isSelected = this.selected[i] === i ? 'isSelected' : ''
 
 			return (
-				<tr key={i} className={isError + ' ' + isSelected}>{td(obj, i)}</tr>
+				<tr key={i} className={isError + ' ' + isTotal + ' ' + isSelected}>{td(obj, i)}</tr>
 			)
 		})
 
