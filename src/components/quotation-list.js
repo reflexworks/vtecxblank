c354a86cf92443905478c7a68f52528481b6cc74
vtec_morishita <vtec_morishita@vtec-morishita-no-MacBook-Air.local>
2018-01-19 12:27:30 +0900

    請求書一覧の項目変更と請求書作成画面の項目追加 #325(不具合修正2)

--- src/components/quotation-list.js
+++ src/components/quotation-list.js
@@ -207,7 +207,7 @@ export default class QuotationList extends React.Component {
 							edit={(data) => this.onSelect(data)}
 							remove={(data) => this.onDelete(data)}
 							header={[{
-								field: 'btn1', title:'請求書を作成する', width: '80px',
+								field: 'btn1', title:'請求書作成', width: '80px',
 								label: <Glyphicon glyph="open-file" />,
 								onClick: (_data) => this.moveInvoiceRegistration(_data)
 							},{
