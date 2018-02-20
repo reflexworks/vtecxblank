917ebffba77ee218850ca42950241ca3368562f9
takezaki <takezaki@virtual-tech.net>
2018-02-20 20:38:48 +0900

    getChargeByZoneまで

--- src/server/put-billing.js
+++ src/server/put-billing.js
@@ -9,36 +9,28 @@ const encoding = 'UTF-8'
 
 // CSV取得
 const result = vtecxapi.getCsv(header, items, parent, skip, encoding)
-vtecxapi.log(JSON.stringify(result))
+//vtecxapi.log(JSON.stringify(result))
 
 const shipment_class = '0'  // 出荷
-const delivery_company_code = 'YM' // ヤマト運輸
+const shipment_service_code = 'YH' // ヤマト運輸
 //const shipment_service_type = 0 // 発払
-//const shipment_service_code = 'YH' // customerのshipment_service_codeは[YM]か[EC]固定
-
-const customer_all = vtecxapi.getFeed('/customer',true)
 
 const shipper_code = result.feed.entry[0].billing.shipper_code
+const size = result.feed.entry[0].billing.size
+const prefecture = result.feed.entry[0].billing.prefecture
+const shipment_service = vtecxapi.getEntry('/shipment_service/YH')
+const customer_all = vtecxapi.getFeed('/customer',true)
 
-// 荷主コード(分類コード)からcustomerを検索
-let customer = customer_all.feed.entry.filter((entry) => {
-	if (entry.customer.shipper&&entry.customer.shipper.length>0) {
-		const result1 = entry.customer.shipper.filter((shipper) => {
-			if (shipper.delivery_company_code !== delivery_company_code) {
-				return false
-			}
-			const result2 = shipper.shipper_info.filter((shipper_info) => { 
-				if ((shipper_info.shipper_code === shipper_code)&&
-					(shipper_info.shipment_class === shipment_class))
-					return true				
-			})
-			return result2.length>0
-		})
-		return result1.length>0
-	}
-})
+//vtecxapi.log('size='+size)
 
-vtecxapi.log('len='+JSON.stringify(customer))
+try {
+	const charge_by_zone = getChargeByZone(shipment_service,shipment_service_code,size,prefecture,customer_all,shipper_code,shipment_class)
+	vtecxapi.log(JSON.stringify(charge_by_zone))
+} catch (e) {
+	vtecxapi.sendError(400,e)
+}
+
+//vtecxapi.log('customer_code='+customer[0].customer.customer_code)
 
 /*
  shipment_service_code
@@ -57,17 +49,61 @@ vtecxapi.log('len='+JSON.stringify(customer))
  delivery_charge
  */
 
-/*
-let reqdata = {
-	'feed': {
-		'entry': []
-	}
+function getChargeByZone(shipment_service,shipment_service_code,size,prefecture,customer_all,shipper_code,shipment_class) {
+
+	const delivery_charge_all = getDeliverycharge(customer_all,shipment_service_code, shipper_code, shipment_class)
+
+	if (!delivery_charge_all) throw 'deliverycharge is not found.'
+	// shipment_service_codeからdelivery_chargeを取得
+	const delivery_charge = delivery_charge_all.feed.entry[0].delivery_charge.filter((delivery_charge) => {
+		return delivery_charge.shipment_service_code === shipment_service_code
+	})
+
+	if (delivery_charge.length === 0) throw 'deliverycharge of '+shipment_service_code+' is not found.'
+	// sizeからdelivery_charge_detailsを取得
+	const delivery_charge_details = delivery_charge[0].delivery_charge_details.filter((delivery_charge_details) => {
+		return delivery_charge_details.size.match(/\d+/)[0] === size
+	})
+
+	// 県からzoneを検索
+	const zone = shipment_service.feed.entry[0].zone.filter((zone) => {
+		const invoice_zones = zone.invoice_zones.filter((invoice_zones) => {
+			return invoice_zones.invoice_zone === prefecture
+		})
+		return invoice_zones.length>0
+	 })
+
+	if (zone.length===0) throw 'zone of '+prefecture+' is not defined.'
+	//	vtecxapi.log('zone_name=' + zone[0].zone_name)			
+
+	if (delivery_charge_details.length === 0) throw 'delivery_charge_details of '+size+' size is not found.'
+	// zone_nameからcharge_by_zoneを取得
+	const charge_by_zone = delivery_charge_details[0].charge_by_zone.filter((charge_by_zone) => {
+		return charge_by_zone.zone_name === zone[0].zone_name
+	})
+
+	return charge_by_zone
+	
 }
 
-result.feed.entry.map( csventry => {
-	reqdata.feed.entry.push(entry)
-})
-vtecxapi.put(reqdata)
-*/
-
-
+function getDeliverycharge(customer_all,shipment_service_code,shipper_code,shipment_class) {
+	// 荷主コード(分類コード)からcustomerを検索
+	const customer = customer_all.feed.entry.filter((entry) => {
+		if (entry.customer.shipper&&entry.customer.shipper.length>0) {
+			const result1 = entry.customer.shipper.filter((shipper) => {
+				if (shipper.shipment_service_code !== shipment_service_code) {
+					return false
+				}
+				const result2 = shipper.shipper_info.filter((shipper_info) => { 
+					if ((shipper_info.shipper_code === shipper_code)&&
+					(shipper_info.shipment_class === shipment_class))
+						return true				
+				})
+				return result2.length>0
+			})
+			return result1.length>0
+		}
+	})
+	if (customer.length===0) throw 'customer code of shipper_code "'+shipper_code+'" is not found.'
+	return vtecxapi.getEntry('/customer/' + customer[0].customer.customer_code + '/deliverycharge')	
+}
