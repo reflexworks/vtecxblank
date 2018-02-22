2a5043450d1c042d44c830622ac0f0b042839492
takezaki <takezaki@virtual-tech.net>
2018-02-22 23:18:57 +0900

    put-billing 実装済

--- src/server/put-billing.js
+++ src/server/put-billing.js
@@ -4,56 +4,74 @@ const items = ['tensyo_code', 'customer_code', 'shipper_code', 'shipping_date', 
 const header = ['店所コード','お客様コード','分類コード','受付日','原票No.','商品区分1','商品区分2','サイズ','個数','扱店都道府県','扱店市区町村','運賃合計','運賃','立替金','保険料','消費税']
 const parent = 'billing'
 const skip = 0
-//const encoding = 'SJIS'
 const encoding = 'UTF-8'
 
 // CSV取得
-const result = vtecxapi.getCsv(header, items, parent, skip, encoding)
+const billingcsv = vtecxapi.getCsv(header, items, parent, skip, encoding)
 //vtecxapi.log(JSON.stringify(result))
 
-const shipment_class = '0'  // 出荷
-const shipment_service_code = 'YH' // ヤマト運輸
-//const shipment_service_type = 0 // 発払
+let shipment_class   		// 出荷(0)or集荷(1)、見つかった荷主コードによって
+let shipment_service_code   // 見つかった荷主コードによって
+let customer_code   		// 見つかった荷主コードによって
+const shipment_service_type = 0 // 発払
 
-const shipper_code = result.feed.entry[0].billing.shipper_code
-const size = result.feed.entry[0].billing.size
-const prefecture = result.feed.entry[0].billing.prefecture
-const shipment_service = vtecxapi.getEntry('/shipment_service/YH')
+let shipment_service = null
 const customer_all = vtecxapi.getFeed('/customer',true)
 
 //vtecxapi.log('size='+size)
+const result = { 'feed': { 'entry': [] } }
 
-try {
-	const charge_by_zone = getChargeByZone(shipment_service,shipment_service_code,size,prefecture,customer_all,shipper_code,shipment_class)
-	vtecxapi.log(JSON.stringify(charge_by_zone))
-} catch (e) {
-	vtecxapi.sendError(400,e)
-}
+billingcsv.feed.entry.map((entry) => {
 
-//vtecxapi.log('customer_code='+customer[0].customer.customer_code)
+	try {
+		const charge_by_zone = getChargeByZone( entry.billing.size, entry.billing.prefecture, entry.billing.shipper_code)
+		if (charge_by_zone.length===0) throw 'charge_by_zone is not found.'
 
-/*
- shipment_service_code
- shipment_service_type 
- shipment_service_service_name 
- shipment_class
- shipper_code
- shipping_date
- tracking_number
- delivery_class1
- delivery_class2
- size
- quantity
- prefecture
- city
- delivery_charge
- */
+		const tracking_number = entry.billing.tracking_number.replace(/-/g,'')
 
-function getChargeByZone(shipment_service,shipment_service_code,size,prefecture,customer_all,shipper_code,shipment_class) {
+		const billing_data = {
+			billing_data: {
+				'shipment_service_code': shipment_service_code,
+				'shipment_service_type': shipment_service_type,
+				'customer_code': customer_code,
+				'shipment_class': shipment_class,
+				'shipper_code': entry.billing.shipper_code,
+				'shipping_date': getFullDate(entry.billing.shipping_date),
+				'tracking_number': tracking_number,
+				'delivery_class1': entry.billing.delivery_class1,
+				'delivery_class2': entry.billing.delivery_class2,
+				'size': entry.billing.size,
+				'prefecture': entry.billing.prefecture,
+				'zone_name': charge_by_zone[0].zone_name,
+				'city': entry.billing.city,
+				'delivery_charge_org_total': entry.billing.delivery_charge_org_total,
+				'delivery_charge': charge_by_zone[0].price,
+			},
+			'link': [
+				{ '___rel': 'self' , '___href': '/billing_data/' + getKey(entry.billing.shipping_date, tracking_number) }
+			]
+		}
 
-	const delivery_charge_all = getDeliverycharge(customer_all,shipment_service_code, shipper_code, shipment_class)
+		result.feed.entry.push(billing_data)
 
-	if (!delivery_charge_all) throw 'deliverycharge is not found.'
+	} catch (e) {
+		vtecxapi.sendError(400, e)
+	}
+
+})	
+
+// datastoreを更新
+vtecxapi.put(result,true)
+
+function getChargeByZone(size,prefecture,shipper_code) {
+
+	const delivery_charge_all = getDeliverycharge(customer_all, shipper_code)
+
+	if (!shipment_service) {
+		shipment_service = vtecxapi.getEntry('/shipment_service/' + shipment_service_code)
+		if (!shipment_service.feed.entry) throw 'shipment_service is not found.(shipment_service_code='+shipment_service_code+')'
+	} 
+
 	// shipment_service_codeからdelivery_chargeを取得
 	const delivery_charge = delivery_charge_all.feed.entry[0].delivery_charge.filter((delivery_charge) => {
 		return delivery_charge.shipment_service_code === shipment_service_code
@@ -86,24 +104,54 @@ function getChargeByZone(shipment_service,shipment_service_code,size,prefecture,
 	
 }
 
-function getDeliverycharge(customer_all,shipment_service_code,shipper_code,shipment_class) {
+function getDeliverycharge(customer_all, shipper_code) {
 	// 荷主コード(分類コード)からcustomerを検索
 	const customer = customer_all.feed.entry.filter((entry) => {
 		if (entry.customer.shipper&&entry.customer.shipper.length>0) {
 			const result1 = entry.customer.shipper.filter((shipper) => {
-				if (shipper.shipment_service_code !== shipment_service_code) {
-					return false
-				}
 				const result2 = shipper.shipper_info.filter((shipper_info) => { 
-					if ((shipper_info.shipper_code === shipper_code)&&
-					(shipper_info.shipment_class === shipment_class))
+					if (shipper_info.shipper_code === shipper_code)
+					{
+						shipment_class = shipper_info.shipment_class // グローバル変数にセット
+						shipment_service_code = shipper.shipment_service_code // グローバル変数にセット
 						return true				
+					}	
 				})
 				return result2.length>0
 			})
 			return result1.length>0
 		}
 	})
-	if (customer.length===0) throw 'customer code of shipper_code "'+shipper_code+'" is not found.'
-	return vtecxapi.getEntry('/customer/' + customer[0].customer.customer_code + '/deliverycharge')	
+	if (customer.length === 0) throw 'customer code of shipper_code "' + shipper_code + '" is not found.'
+	customer_code = customer[0].customer.customer_code
+	const deliverycharge = vtecxapi.getEntry('/customer/' + customer[0].customer.customer_code + '/deliverycharge')	
+	if (!deliverycharge.feed.entry) throw 'deliverycharge is not found. (customer code=' + customer[0].customer.customer_code +')'
+	return deliverycharge
+}
+
+function getFullDate(datestr) {
+	const matches = /^(\d+)月(\d+)日$/.exec(datestr)
+	const now = new Date()
+	const month = parseInt(matches[1])
+	const day = parseInt(matches[2])
+	let year = now.getFullYear()
+
+	if (now.getMonth()+1 < month ) {
+		year = year - 1
+	}
+	return year+'-'+month+'-'+day
+
+}
+
+function getKey(datestr, tracking_number) {
+
+	const matches = /^(\d+)月(\d+)日$/.exec(datestr)
+	const now = new Date()
+	const month = parseInt(matches[1])
+	let year = now.getFullYear()
+
+	if (now.getMonth() + 1 < month) {
+		year = year - 1
+	}
+	return year + ('0' + month).slice(-2) + tracking_number
 }
