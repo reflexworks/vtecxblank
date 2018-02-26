748ec610e922fb0c2a32cbac21e1ae1162aab9f9
takeyama <keigo@virtual-tech.net>
2018-02-26 15:32:51 +0900

    見積書PDF>配送料 #469

---
+++ src/pdf/deliverychargestyles.js
@@ -0,0 +1,153 @@
+export const _page = {
+	pagesize: 'A4',
+	orientation: 'landscape',
+	left: 0,
+	right: 0,
+	top: 0,
+	bottom: 0,
+	linecolor: '#000000'
+}
+
+export const page_title_table = {
+	widths: '100',
+	width: '100%'
+}
+
+export const page_title = {
+	size: 18,
+	align: 'center',
+	space: 10,
+	height: 60,
+	offsety: 20
+}
+export const page_title_blank = {
+	size: 6,
+	leading: 50,
+	space: 1
+}
+
+export const header_table = {
+	width: '100%',
+	widths: '10,40,20,20,10',
+	bordercolor: '#000000',
+	'border-spacing': '1px'
+}
+
+export const header_title = {
+	size: 18,
+	left: 'false',
+	right: 'false',
+	top: 'false',
+	bottom: 'true',
+	offsety: 5
+}
+
+export const header_comment = {
+	size: 6,
+	leading: 9,
+	space: 1
+}
+
+export const billfrom_info = {
+	size: 6,
+	leading: 10,
+	space: 1
+}
+
+
+export const remark_table = {
+	width: '100%',
+	widths: '10,80,10',
+	bordercolor: '#000000',
+	'border-spacing': '1px'
+}
+export const remark = {
+	size: 6,
+	leading: 15,
+	space: 1,
+	offsety: 3,
+	left: 'false',
+	right: 'false',
+	top: 'false',
+	bottom: 'true'
+}
+export const remark_table_top_blank = {
+	size: 6,
+	leading: 50,
+	space: 1
+}
+
+export const header_table_sub = {
+	width: '100%',
+	widths: '10,80,10',
+	bordercolor: '#000000',
+	'border-spacing': '1px'
+}
+
+export const header_title_sub = {
+	size: 7,
+	left: 'false',
+	right: 'false',
+	top: 'false',
+	bottom: 'true',
+	offsety: 5,
+	height: 30
+}
+export const deliverycharge_title = {
+	size: 6,
+	leading: 10,
+	space: 1,
+	height: 7
+}
+export const deliverycharge_title_table_top_blank = {
+	size: 6,
+	leading: 6,
+	space: 1
+}
+
+export const deliverycharge_table_th = {
+	bgcolor: '#cccccc',
+	size: 6,
+	leading: 10,
+	space: 1,
+	offsety: 2,
+	align: 'center',
+	left:'true',
+	right:'true',
+	top:'true',
+	bottom:'true'
+}
+export const deliverycharge_table_th_blank = {
+	size: 6,
+	leading: 10,
+	space: 1,
+	offsety: 2,
+	left:'false',
+	right:'false',
+	top:'false',
+	bottom:'false'
+}
+export const deliverycharge_table_td = {
+	size: 6,
+	leading: 10,
+	space: 1,
+	offsety: 2,
+	align: 'right',
+	left:'true',
+	right:'true',
+	top:'true',
+	bottom:'true'
+}
+
+export const deliverycharge_table_td_note = {
+	size: 6,
+	leading: 10,
+	space: 1,
+	offsety: 2,
+	offsetx: 2,
+	align: 'left',
+	left:'true',
+	right:'true',
+	top:'true',
+	bottom:'true'
+}
