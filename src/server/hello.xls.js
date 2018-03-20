91df21f9164c622aed4ea9f5c61b3978a57d9a84
takezaki <takezaki@virtual-tech.net>
2018-03-20 17:56:41 +0900

    不要なファイルを消去

--- src/server/hello.xls.js
+++
@@ -1,7 +0,0 @@
-import vtecxapi from 'vtecxapi' 
-
-const data = {'feed' : {'entry' : [{'userinfo' : {'id' : 123,'email' : 'foo@bar.com'}},{'favorite' : {'food' : 'ラーメン','music' : ['ジャズ','ポップス','ロック']}}]}}
-
-// XLS出力
-vtecxapi.toXls(data, '/xls/person_template.xls', 'test.xls')
-
