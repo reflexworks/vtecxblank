429f965c648ee5ac73aa488c0356a29c063d818d
takeyama <keigo@virtual-tech.net>
2018-04-16 14:15:27 +0900

    検索時にindexを貼り直す修正

--- src/components/common.js
+++ src/components/common.js
@@ -2392,9 +2392,16 @@ export class CommonPagination extends React.Component {
 		const new_url = newProps.url
 		if (this.url !== new_url) {
 			// pageIndex作成処理呼び出し
+			this.pageIndex = 0
 			this.handleSelect(1, new_url)
 			this.url = new_url
 		}
+
+		let activePage = 1
+		if (newProps.activePage) {
+			activePage = newProps.activePage
+		}
+
 		// 件数取得
 		axios({
 			url: new_url + '&c',
@@ -2404,10 +2411,10 @@ export class CommonPagination extends React.Component {
 			}
 		}).then((response) => {
 			this.resultcount = Number(response.data.feed.title)
-			const items =  Math.ceil(this.resultcount / this.props.maxDisplayRows)
-			this.setState({ items: items })
+			const items = Math.ceil(this.resultcount / this.props.maxDisplayRows)
+			this.setState({ items: items, activePage: activePage })
 		}).catch(() => {
-			this.setState({ items: 0 })
+			this.setState({ items: 0, activePage: activePage })
 		})
 	}
 
@@ -2935,11 +2942,19 @@ export function CommonBackInternalWork(_befor_url) {
 }
 
 import { setTimeout } from 'timers'
-export function CommonGetList(_url, _activePage) {
+export function CommonGetList(_url, _activePage, _conditionsKey) {
 	return new Promise((resolve) => {
+		let retryCount = 0
+		const maxRetryCount = 10
 		const get = () => {
+
+			const url = _url + '&n=' + _activePage
+
+			// 今回の検索条件を保存する
+			CommonBeforConditions().set(_conditionsKey, url)
+
 			axios({
-				url: _url + '&n=' + _activePage,
+				url: url,
 				method: 'get',
 				headers: {
 					'X-Requested-With': 'XMLHttpRequest'
@@ -2958,9 +2973,14 @@ export function CommonGetList(_url, _activePage) {
 				if (error.response && error.response.data && error.response.data.feed) {
 					const title = error.response.data.feed.title
 					if (title === 'Please make a pagination index in advance.') {
-						setTimeout(() => {
-							get()
-						}, 200)
+						if (retryCount > maxRetryCount) {
+							setTimeout(() => {
+								retryCount++
+								get()
+							}, 500)
+						} else[
+							alert('一覧のindex作成に取得に失敗しました。\n\n' + title)
+						]
 					}
 				} else {
 					resolve({ isDisabled: false, isError: error })
@@ -2969,4 +2989,19 @@ export function CommonGetList(_url, _activePage) {
 		}
 		get()
 	})
+}
+let _commonBeforConditions = {}
+export function CommonBeforConditions() {
+	return {
+		set: (_key, _conditions) => {
+			return _commonBeforConditions[_key] = _conditions
+		},
+		get: (_key, _url) => {
+			let res = null
+			if (_commonBeforConditions[_key]) {
+				res = _commonBeforConditions[_key].replace(_url, '')
+			}
+			return res
+		}
+	}
 }
