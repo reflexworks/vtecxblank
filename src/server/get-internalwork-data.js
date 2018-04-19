829426683f69a2cdff75b0bba59aadf128cd1b2d
takezaki <takezaki@virtual-tech.net>
2018-04-19 18:55:22 +0900

    本番機対応 #895

--- src/server/get-internalwork-data.js
+++ src/server/get-internalwork-data.js
@@ -4,7 +4,7 @@ export function getInternalworkdata(working_yearmonth,customer_code,quotation_co
 
 	const internal_work = vtecxapi.getFeed('/internal_work?customer.customer_code=' + customer_code + '&quotation.quotation_code=' + quotation_code + '&internal_work.working_yearmonth=' + working_yearmonth)
 
-	if (internal_work.feed.entry) {
+	if (internal_work) {
 		const id = internal_work.feed.entry[0].id.split(',')[0]
 		//		if (internal_work.feed.entry[0].internal_work.is_completed==='0') throw '完了がなされていません'
 		const internal_work_all = vtecxapi.getFeed(id + '/data')
