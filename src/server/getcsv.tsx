109220794f5faccc3763143ed9bba40258d64ce1
takezaki <takezaki@virtual-tech.net>
2018-07-15 15:36:59 +0900

    csvアップロード

---
+++ src/server/getcsv.tsx
@@ -0,0 +1,12 @@
+import * as vtecxapi from 'vtecxapi'
+
+const items = ['item1', 'item2(int)', 'item3(int)']
+const header = ['年月日', '件数', '合計']
+const parent = 'order'
+const skip = 1
+//const encoding = 'Windows-31J'
+const encoding = 'UTF-8'
+
+// CSV取得
+const result = vtecxapi.getCsv(header, items, parent, skip, encoding)
+vtecxapi.log(JSON.stringify(result)) // {"feed":{"entry":[{"order":{"item1":"2017/7/5","item2":3,"item3":3}},{"order":{"item1":"2017/7/6","item2":5,"item3":8}},{"order":{"item1":"2017/7/7","item2":2,"item3":10}}]}}
