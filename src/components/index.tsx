f3a37a69e2e68c2842e6a8e2546a81a8c84b651e
takezaki <takezaki@virtual-tech.net>
2019-07-09 15:14:16 +0900

    modify index Hello vte.cx!

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>test</div>, document.getElementById('container'))
+ReactDOM.render(<div>Hello vte.cx!</div>, document.getElementById('container'))
